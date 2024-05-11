96f4fbdab0e4a3f6baa2dad3faf2845a36426aa2
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-11 05:09:06 +0200

    demo5

--- index.js
+++ index.js
@@ -1,60 +1,46 @@
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/person.js')
-const mongoose = require('mongoose')
+const Person = require('./models/person')
 
 require('dotenv').config()
 
 const app = express()
-
-morgan.token('post_info', function (req, res) {
-    if (req.method === 'POST') {
-        return JSON.stringify(req.body)
-    } else return null
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_info'))
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-const password = process.argv[2]
-const url = process.env.MONGODB_URI
 const PORT = process.env.PORT || 3001
 
-mongoose.set('strictQuery',false)
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-  
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (req, res) => {
   Person.find({})
-  .then(persons => {
-    if (persons.length > 0) {
-      response.json(persons)
-    } else {
-      response.status(404).end()
-    }
-  })
+    .then(persons => {
+      if (persons.length > 0) {
+        res.json(persons)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching persons:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
 })
 
-app.get('/info', async (request,response) => {
-    let personNo = await Person.countDocuments({})
-    let dateTime = new Date().toString()
-    dateTime = dateTime .substring(0, 25)
-    response.send(`Phonebook has info for ${personNo} people <br/> <br/>
-    ${dateTime} GMT+0300 Eastern European Summer Time`)
+app.get('/info', async (req, res) => {
+  try {
+    const personCount = await Person.countDocuments({})
+    const dateTime = new Date().toString().substring(0, 25)
+    res.send(`Phonebook has info for ${personCount} people <br/> <br/> ${dateTime} GMT+0300 Eastern European Summer Time`)
+  } catch (error) {
+    console.error('Error fetching person count:', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
 })
 
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,17 +1,28 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB')
+})
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    id: String
-  })
+  name: String,
+  number: String,
+  id: String
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
- module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema)
