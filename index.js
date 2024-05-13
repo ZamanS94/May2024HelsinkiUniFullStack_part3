a388b6f5825901ca6991088bdc79d022674fd240
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-13 19:12:41 +0200

    3.16: Phonebook database, step 4

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const Person = require('./models/person')
 
 require('dotenv').config()
 
+morgan.token('post_info', function (req, res) {
+  if (req.method === 'POST') {
+      return JSON.stringify(req.body)
+  } else return null
+})
+
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_info'))
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -17,13 +23,19 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-  Person.find({})
-    .then(persons => {
-      if (persons.length > 0) {
-        res.json(persons)
-      } else res.status(404).end()
-    })
+app.get('/api/persons', async (req, res, next) => {
+  try {
+    const persons = await Person.find({})
+    if (persons.length > 0) {
+      res.json(persons)
+    } else {
+      const error = new Error('Persons not found')
+      error.status = 404
+      throw error
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 app.get('/info', async (req, res) => {
@@ -32,29 +44,45 @@ app.get('/info', async (req, res) => {
   res.send(`Phonebook has info for ${personCount} people <br/> <br/> ${dateTime} GMT+0300 Eastern European Summer Time`)
 })
 
-app.post('/api/persons', async (request, response) => {
-
-  const body = request.body
-  if (!body.name || !body.number) return response.status(400).json({ error: "name or number missing" })
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  person.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      console.log(result)
-      response.status(204).end()
+app.post('/api/persons', async (request, response, next) => {
+  try {
+    const body = request.body
+    if (!body.name || !body.number) {
+      const error = new Error('Name or number missing')
+      error.status = 400
+      throw error
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
+    const savedPerson = await person.save()
+    response.json(savedPerson)
+  } catch (error) {
+    next(error)
+  }
 })
+
+app.delete('/api/persons/:id', async (request, response, next) => {
+  try {
+    const result = await Person.findByIdAndDelete(request.params.id)
+    if (result) {
+      response.status(204).end()
+    } else {
+      const error = new Error('Person not found')
+      error.status = 404
+      throw error
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+const errorHandler = (err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message })
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
